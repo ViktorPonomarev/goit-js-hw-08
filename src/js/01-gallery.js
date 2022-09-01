d9ba68df3370270db19c9d0975b1ba5140ffd9ac
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-09-01 13:09:36 +0300

    олмгп

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,49 +1,49 @@
-// Описан в документации
-import SimpleLightbox from "simplelightbox";
-// Дополнительный импорт стилей
-import "simplelightbox/dist/simple-lightbox.min.css";
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// // Описан в документации
+// import SimpleLightbox from "simplelightbox";
+// // Дополнительный импорт стилей
+// import "simplelightbox/dist/simple-lightbox.min.css";
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
 
-/* 1. Создание и рендер разметки по массиву данных 
-galleryItems и предоставленному шаблону элемента галереи.
- */
+// /* 1. Создание и рендер разметки по массиву данных 
+// galleryItems и предоставленному шаблону элемента галереи.
+//  */
 
-const imgContainer = document.querySelector('.gallery');
-const cardsMarkup = createGalleryCardsMarkup(galleryItems);
-imgContainer.insertAdjacentHTML('beforeend', cardsMarkup);
-// imgContainer.addEventListener('click', onImgContainerClick);
+// const imgContainer = document.querySelector('.gallery');
+// const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+// imgContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+// // imgContainer.addEventListener('click', onImgContainerClick);
 
-function createGalleryCardsMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
+// function createGalleryCardsMarkup(galleryItems) {
+//     return galleryItems.map(({ preview, original, description }) => {
 
-        return `
-        <a class="gallery__item" 
-        href="${original}">
-  <img class="gallery__image" 
-  src="${preview}" 
-  alt="${description}" 
-  />
-</a>
-        `;
-    })
-        .join('');
-}
+//         return `
+//         <a class="gallery__item" 
+//         href="${original}">
+//   <img class="gallery__image" 
+//   src="${preview}" 
+//   alt="${description}" 
+//   />
+// </a>
+//         `;
+//     })
+//         .join('');
+// }
 
-/* 5. Посмотри в документации секцию «Options» и добавь отображение
- подписей к изображениям из атрибута alt. Пусть подпись будет 
- снизу и появляется через 250 миллисекунд после открытия изображения. 
- */
-var lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: "alt",
-    captionDelay: 250,
-    captionPosition: 'bottom',
-    // nav: true,
-    // close: true,
-    // caption: true,
+// /* 5. Посмотри в документации секцию «Options» и добавь отображение
+//  подписей к изображениям из атрибута alt. Пусть подпись будет 
+//  снизу и появляется через 250 миллисекунд после открытия изображения. 
+//  */
+// var lightbox = new SimpleLightbox('.gallery a', {
+//     captionsData: "alt",
+//     captionDelay: 250,
+//     captionPosition: 'bottom',
+//     // nav: true,
+//     // close: true,
+//     // caption: true,
     
-});
+// });
 
 
-console.log(galleryItems);
+// console.log(galleryItems);
