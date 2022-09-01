f350d016be4a69295977d4a81b605b364ab078ec
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-09-01 14:07:26 +0300

    егныкрмтот

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,5 @@ import throttle from 'lodash.throttle';
 const throttle = require('lodash.throttle');
 
     const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
-
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+  
 
